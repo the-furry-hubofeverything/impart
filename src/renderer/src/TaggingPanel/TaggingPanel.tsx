6ae149a1ef472b37f5b0022013de64c1b3bfd6e1
Arastryx <arastryx.dravoga@gmail.com>
2024-07-04 20:04:17 -0300

    Added Some Nice Buttons and Stuff 👉👈

--- src/renderer/src/TaggingPanel/TaggingPanel.tsx
+++ src/renderer/src/TaggingPanel/TaggingPanel.tsx
@@ -8,7 +8,9 @@ import {
   Typography,
   Divider,
   Grid,
-  Chip
+  Chip,
+  IconButton,
+  TextField
 } from '@mui/material'
 import { useFileIndexStatus } from '../FileIndexStatusProvider'
 import { IndexingPanel } from './IndexingPanel'
@@ -26,25 +28,50 @@ export function TaggingPanel({}: TaggingPanelProps) {
     <Stack width="100%" height="100%">
       <Card sx={{ flex: 1 }}>
         <CardContent>
-          {groups?.map((g) => (
-            <Box key={g.id}>
-              <Typography variant="h5">{g.label}</Typography>
-              <Divider />
-              <Grid container pt={1} pb={3} spacing={1}>
-                {g.tags.map((t) => (
-                  <Grid key={t.id} item>
-                    <Chip
-                      label={t.label}
-                      sx={(theme) => ({
-                        bgcolor: t.color,
-                        color: theme.palette.getContrastText(t.color)
-                      })}
-                    />
+          <Stack gap={2}>
+            <TextField label="Search" variant="standard" />
+            {groups?.map((g) => (
+              <Box
+                key={g.id}
+                sx={{
+                  '& .MuiIconButton-root': {
+                    opacity: 0,
+                    transition: '0.2s'
+                  },
+                  '&:hover .MuiIconButton-root': {
+                    opacity: 1
+                  }
+                }}
+              >
+                <Typography variant="h5">{g.label}</Typography>
+                <Divider />
+                <Grid container py={1} spacing={1}>
+                  {g.tags.map((t) => (
+                    <Grid key={t.id} item>
+                      <Chip
+                        label={t.label}
+                        sx={(theme) => ({
+                          bgcolor: t.color,
+                          color: theme.palette.getContrastText(t.color),
+                          cursor: 'pointer',
+                          transition: '0.2s',
+
+                          '&:hover': {
+                            opacity: 0.9
+                          }
+                        })}
+                      />
+                    </Grid>
+                  ))}
+                  <Grid item>
+                    <IconButton size="small">
+                      <AddIcon />
+                    </IconButton>
                   </Grid>
-                ))}
-              </Grid>
-            </Box>
-          ))}
+                </Grid>
+              </Box>
+            ))}
+          </Stack>
         </CardContent>
       </Card>
       <Collapse in={isIndexing}>
