1976ffca3fb7db6db95f2c257c2ab47a362ab285
Arastryx <arastryx.dravoga@gmail.com>
2024-11-03 17:58:38 -0400

    Cute Empty Images :)
    -Added an empty message for when no taggables are found along with some cute art to go along with it

---
+++ src/renderer/src/Common/Components/TaggableGrid/EmptyImages/index.ts
@@ -0,0 +1,4 @@
+import empty1 from './NoItemsFound1.png'
+import empty2 from './NoItemsFound2.png'
+
+export const emptyImages = [empty1, empty2]
--- src/renderer/src/Common/Components/TaggableGrid/VirtualTaggableGrid.tsx
+++ src/renderer/src/Common/Components/TaggableGrid/VirtualTaggableGrid.tsx
@@ -1,10 +1,15 @@
-import { Grid2 as Grid, Stack } from '@mui/material'
-import { forwardRef } from 'react'
+import { Box, Grid2 as Grid, Stack, Typography } from '@mui/material'
+import { forwardRef, useEffect, useState } from 'react'
 import { GridComponents, VirtuosoGrid } from 'react-virtuoso'
 import { BOX_WIDTH } from '../TaggableDisplay/TaggableDisplay'
 import { useEditTaggable } from '@renderer/TaggableBrowser/EditTaggableProvider'
 import { useScrollLock } from '../../Hooks/useScrollLock'
 import { TaggableWrapper, TaggableWrapperProps } from './TaggableWrapper'
+import { emptyImages } from './EmptyImages'
+
+function getRandomEmptyImage() {
+  return emptyImages[Math.floor(Math.random() * emptyImages.length)]
+}
 
 const gridComponents: GridComponents = {
   List: forwardRef(({ children, ...props }, ref) => (
@@ -28,8 +33,29 @@ export function VirtualTaggableGrid({ taggables, ...wrapperProps }: VirtualTagga
     return null
   }
 
+  const [emptyImage, setEmptyImage] = useState(getRandomEmptyImage())
+
+  useEffect(() => {
+    //To prevent flickering, we only change the image if there ARE taggables
+    // rather than if there aren't
+    if (taggables.length > 0) {
+      setEmptyImage(getRandomEmptyImage())
+    }
+  }, [taggables])
+
   const editState = useEditTaggable()
   const handleScroll = useScrollLock(editState && editState.editTarget != null)
+
+  if (taggables.length === 0) {
+    return (
+      <Stack width="100%" height="100%" alignItems="center" justifyContent="center" gap={0}>
+        <img src={emptyImage} height={400} />
+        <Typography color="primary.main" fontSize={36}>
+          No Items Found
+        </Typography>
+      </Stack>
+    )
+  }
 
   return (
     <VirtuosoGrid
