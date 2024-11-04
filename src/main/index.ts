2b9ac40fe38ec1293906f03fbd76256911761197
Arastryx <arastryx.dravoga@gmail.com>
2024-11-04 18:36:17 -0400

    Multiple Instance Prevention
    -Added code to prevent opening multiple instances of the app

--- src/main/index.ts
+++ src/main/index.ts
@@ -69,55 +69,73 @@ function createWindow(): void {
   impartApp.mainWindow = mainWindow
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(async () => {
-  // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+const gotTheLock = app.requestSingleInstanceLock()
 
-  protocol.handle('thum', async (request) => {
-    let params = request.url.slice('thum:///'.length)
-
-    const [taggableId] = params.split('/')
-
-    return net.fetch(
-      url.pathToFileURL((await ThumbnailManager.getThumbnail(Number(taggableId))) ?? '').toString()
-    )
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (impartApp.mainWindow) {
+      if (impartApp.mainWindow.isMinimized()) {
+        impartApp.mainWindow.restore()
+      }
+      impartApp.mainWindow.focus()
+    }
   })
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.whenReady().then(async () => {
+    // Set app user model id for windows
+    electronApp.setAppUserModelId('com.electron')
+
+    protocol.handle('thum', async (request) => {
+      let params = request.url.slice('thum:///'.length)
+
+      const [taggableId] = params.split('/')
+
+      return net.fetch(
+        url
+          .pathToFileURL((await ThumbnailManager.getThumbnail(Number(taggableId))) ?? '')
+          .toString()
+      )
+    })
+
+    // Default open or close DevTools by F12 in development
+    // and ignore CommandOrControl + R in production.
+    // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+    app.on('browser-window-created', (_, window) => {
+      optimizer.watchWindowShortcuts(window)
+    })
+
+    await AppDataSource.initialize()
+
+    setupFileApi()
+    setupTaggableApi()
+    setupStackApi()
+    setupTagApi()
+    setupIndexApi()
+
+    createWindow()
+
+    app.on('activate', function () {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
   })
 
-  await AppDataSource.initialize()
-
-  setupFileApi()
-  setupTaggableApi()
-  setupStackApi()
-  setupTagApi()
-  setupIndexApi()
-
-  createWindow()
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  // Quit when all windows are closed, except on macOS. There, it's common
+  // for applications and their menu bar to stay active until the user quits
+  // explicitly with Cmd + Q.
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
   })
-})
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
+  // In this file you can include the rest of your app"s specific main process
+  // code. You can also put them in separate files and require them here.
+}
